a96251a6c2d95da2e7cfa11d81dd185d8bdcd521
HeikkiDev <enri46@gmail.com>
2018-08-05 14:18:05 +0200

    Platform-Specific iOS function to open Settings

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,13 +1,43 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
+#import <Flutter/Flutter.h>
 
 @implementation AppDelegate
+- (BOOL)application:(UIApplication*)application didFinishLaunchingWithOptions:(NSDictionary*)launchOptions {
+    FlutterViewController* controller = (FlutterViewController*)self.window.rootViewController;
+    
+    FlutterMethodChannel* flutterChannel = [FlutterMethodChannel
+                                            methodChannelWithName:@"com.heikkidev.flutterplatformspecific/nativeservices"
+                                            binaryMessenger:controller];
+    
+    [flutterChannel setMethodCallHandler:^(FlutterMethodCall* call, FlutterResult result) {
+        if ([@"openSystemSettings" isEqualToString:call.method]) {
+            bool opneOk = [self openSystemSettings];
+            
+            if (opneOk == false) {
+                result([FlutterError errorWithCode:@"UNAVAILABLE"
+                                           message:@"Cannot open system settings."
+                                           details:nil]);
+            } else {
+                result(@(opneOk));
+            }
+        } else {
+            result(FlutterMethodNotImplemented);
+        }
+    }];
+    
+    return [super application:application didFinishLaunchingWithOptions:launchOptions];
+}
 
-- (BOOL)application:(UIApplication *)application
-    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-  [GeneratedPluginRegistrant registerWithRegistry:self];
-  // Override point for customization after application launch.
-  return [super application:application didFinishLaunchingWithOptions:launchOptions];
+- (bool)openSystemSettings {
+    @try {
+        [[UIApplication sharedApplication] openURL:[NSURL URLWithString:UIApplicationOpenSettingsURLString]];
+        return true;
+    }
+    @catch (NSException *exception) {
+        NSLog(@"%@", exception.reason);
+        return false;
+    }
 }
 
 @end
